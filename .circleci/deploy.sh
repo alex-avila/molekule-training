65f260cd4686a78a2ae48e7e0f0f096beeb0a6dc
Alex Avila <alex.avilx@icloud.com>
2020-04-13 21:33:14 -0600

    Undo  recent changes

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,11 +7,10 @@ echo "Start deploy"
 # https://blog.e-kursy.it/aws-lambda-edge-brotli/
 # https://medium.com/@felice.geracitano/brotli-compression-delivered-from-aws-7be5b467c2e1
 cd ./.nuxt/dist/
-echo $PWD
 find -L ./client -name '*.js' -o -name '*.json' -o -name '*.svg' -o -name '*.woff' -o -name '*.woff2' \
  | xargs -n 1 -I {} sh -c 'mkdir -p ./gz/`dirname $1` && cp {} ./gz/{} && gzip --best ./gz/{} && mv ./gz/{}.gz ./gz/{}' sh {}
-find -L ./client \( -name '*.js' -o -name '*.json' -o -name '*.svg' -o -name '*.woff' -o -name '*.woff2' \) \
- -exec mkdir -p ./br/client \; -exec cp {} ./br/{} \; -exec brotli -f --best ./br/{} \; -exec mv ./br/{}.br ./br/{} \;
+find -L ./client -name '*.js' -o -name '*.json' -o -name '*.svg' -o -name '*.woff' -o -name '*.woff2' \
+ | xargs -n 1 -I {} sh -c 'mkdir -p ./br/`dirname $1` && cp {} ./br/{} && brotli --best ./br/{} && mv ./br/{}.br ./br/{}' sh {}
 cd ../..
 
 aws s3 cp --recursive --cache-control 'public, max-age=31536000' ./.nuxt/dist/client s3://elasticbeanstalk-us-east-2-110898720018/${CIRCLE_TAG}/_nuxt/
