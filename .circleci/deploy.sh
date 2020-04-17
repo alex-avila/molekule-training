059e0070692e2fe63a1af2f13ffc53f18c1eb366
Alex Avila <alex.avilx@icloud.com>
2020-04-17 15:31:07 -0600

    Update code to deploy to new EB application

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "Start deploy"
-
 # Compress static files with gzip and brotli
 # https://blog.e-kursy.it/aws-lambda-edge-brotli/
 # https://medium.com/@felice.geracitano/brotli-compression-delivered-from-aws-7be5b467c2e1
@@ -13,11 +11,11 @@ find -L ./client -name '*.js' -o -name '*.json' -o -name '*.svg' -o -name '*.wof
  | xargs -n 1 -I {} sh -c 'mkdir -p ./br/`dirname $1` && cp {} ./br/{} && brotli --best ./br/{} && mv ./br/{}.br ./br/{}' sh {}
 cd ../..
 
-aws s3 cp --recursive --cache-control 'public, max-age=31536000' ./.nuxt/dist/client s3://elasticbeanstalk-us-east-2-110898720018/${CIRCLE_BRANCH}/_nuxt/
-aws s3 cp --recursive --cache-control 'public, max-age=31536000' --content-encoding gzip ./.nuxt/dist/gz/client s3://elasticbeanstalk-us-east-2-110898720018/${CIRCLE_BRANCH}/gz/_nuxt/
-aws s3 cp --recursive --cache-control 'public, max-age=31536000' --content-encoding br ./.nuxt/dist/br/client s3://elasticbeanstalk-us-east-2-110898720018/${CIRCLE_BRANCH}/br/_nuxt/
+aws s3 cp --recursive --cache-control 'public, max-age=31536000' ./.nuxt/dist/client s3://elasticbeanstalk-us-east-1-411004368230/${CIRCLE_BRANCH}/_nuxt/
+aws s3 cp --recursive --cache-control 'public, max-age=31536000' --content-encoding gzip ./.nuxt/dist/gz/client s3://elasticbeanstalk-us-east-1-411004368230/${CIRCLE_BRANCH}/gzip/_nuxt/
+aws s3 cp --recursive --cache-control 'public, max-age=31536000' --content-encoding br ./.nuxt/dist/br/client s3://elasticbeanstalk-us-east-1-411004368230/${CIRCLE_BRANCH}/br/_nuxt/
 
 rm -rf node_modules
 yarn --prod
 
-eb deploy $CIRCLE_BRANCH-env
+eb deploy MolekuleTraining-$CIRCLE_BRANCH-env
